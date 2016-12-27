38a726949326ab2762e045eb5eaa8f376bb30172
binex <ipspirid@gmail.com>
2016-12-27 19:16:41 +0500

    Изменил батник запуска обмена. Удалил отладочный код.

--- obmen1c77.bat
+++ obmen1c77.bat
@@ -1,1 +1,2 @@
-"C:\Program Files (x86)\1Cv77\BIN\1cv7s.exe" enterprise /D"C:\Buhg\G\SQL_DEBUG\" /Nediloader /p123
+rem "C:\Program Files (x86)\1Cv77\BIN\1cv7s.exe" enterprise /D"C:\Buhg\G\SQL_DEBUG\" /Nediloader /p123
+"C:\Program Files (x86)\1Cv77\BIN\1cv7s.exe" enterprise /D"C:\Buhg\G\SQLDB\" /Nediloader /p123
